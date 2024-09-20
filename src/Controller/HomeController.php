a4761cd7b7e325bfb369ba8abb57312b99eb4914
potichatmignon <arthur.garnier1090@gmail.com>
2024-09-20 17:44:50 +0200

    connexion page + admin page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
@@ -26,5 +25,38 @@ class HomeController extends AbstractController
     public function contact(): Response
     {
         return $this->render('contact.html.twig');
+    }
+
+    #[Route('/connexion', name: 'app_connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('connexion.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login', methods: ['GET'])]
+    public function login(Request $request): Response
+    {
+        $username = $request->query->get('username');
+        $password = $request->query->get('password');
+    
+        if (!$username || !$password) {
+            return $this->render('connexion.html.twig', [
+                'error' => 'Les champs ne peuvent pas être vides'
+            ]);
+        }
+    
+        if ($username === 'vparrot' && $password === 'admin') {
+            return $this->redirectToRoute('app_admin');
+        }
+    
+        return $this->render('connexion.html.twig', [
+            'error' => 'Nom d\'utilisateur ou mot de passe incorrect'
+        ]);
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function admin(): Response
+    {
+        return $this->render('admin.html.twig');
     }
 }
