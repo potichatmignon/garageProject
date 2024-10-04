9e6c53f4ac8e795b9114129c3aebe5b39f98f64c
Arthur <arthur.garnier1090@gmail.com>
2024-10-04 08:45:36 +0200

    last commit

--- migrations/Version20241004063944.php
+++ migrations/Version20241004063944.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241003210335 extends AbstractMigration
+final class Version20241004063944 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241003210335 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE info (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE avis ADD valid TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE info');
+        $this->addSql('ALTER TABLE avis DROP valid');
     }
 }
